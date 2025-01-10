f268d495be4611d1de050d2796636f538a8de9d5
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-01-10 15:55:25 -0500

    Enable MapleSim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.ironmaple.simulation.SimulatedArena;
 import frc.robot.Constants.LoggingConstants;
 
 /**
@@ -114,5 +115,7 @@ public class Robot extends TimedRobot {
 
 	/** This function is called periodically whilst in simulation. */
 	@Override
-	public void simulationPeriodic() {}
+	public void simulationPeriodic() {
+		SimulatedArena.getInstance().simulationPeriodic();
+	}
 }
