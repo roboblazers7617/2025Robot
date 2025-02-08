1e39b5cbb68cb5a1e105bed3399c893af5b6ee68
A-Person323 <Caleb@thereeds.net>
2025-02-08 13:27:12 -0500

    didn't save all of the changes prior to last update
    now it is all of the ones I commented out probably

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,8 +38,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
 	private SendableChooser<Command> autoChooser = new SendableChooser<>();
 	// The robot's subsystems and commands are defined here...
-	// private final Drivetrain drivetrain = new Drivetrain(DrivetrainConstants.CONFIG_DIR);
-	// private final Dashboard dashboard = new Dashboard(drivetrain, this);
+	private final Drivetrain drivetrain = new Drivetrain(DrivetrainConstants.CONFIG_DIR);
+	private final Dashboard dashboard = new Dashboard(drivetrain, this);
 
 	// Replace with CommandPS4Controller or CommandJoystick if needed
 	@NotLogged
