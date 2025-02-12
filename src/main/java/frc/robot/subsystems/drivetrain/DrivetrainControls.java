50af0c1284c71e53f8e0a3a692beda1ed603e38a
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-02-12 18:32:48 -0500

    Scale driverSteeringKnob values correctly

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainControls.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainControls.java
@@ -73,7 +73,7 @@ public final class DrivetrainControls {
 	 */
 	public static SwerveInputStream driveButtonBox(Drivetrain drivetrain, ButtonBox buttonBox) {
 		return SwerveInputStream.of(drivetrain.getSwerveDrive(), () -> (buttonBox.getDriverJoystick().getY() * 2 - 1), () -> ((buttonBox.getDriverJoystick().getX() * 2 - 1) * -1))
-				.withControllerHeadingAxis(() -> Math.sin(buttonBox.getDriverSteeringKnob().getPosition() * Math.PI) * (Math.PI * 2), () -> Math.cos(buttonBox.getDriverSteeringKnob().getPosition() * Math.PI) * (Math.PI * 2))
+				.withControllerHeadingAxis(() -> Math.sin((buttonBox.getDriverSteeringKnob().getPosition() * 2 - 1) * Math.PI) * (Math.PI * 2), () -> Math.cos((buttonBox.getDriverSteeringKnob().getPosition() * 2 - 1) * Math.PI) * (Math.PI * 2))
 				.deadband(OperatorConstants.DEADBAND)
 				.scaleTranslation(DrivetrainConstants.TRANSLATION_SCALE)
 				.allianceRelativeControl(true)
