ab528b9de7266685f7162a11e99bbb6ead86b371
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2025-02-15 13:24:55 -0500

    remove useless current and voltage data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,18 +25,8 @@ public class Robot extends TimedRobot {
 
 	private final RobotContainer m_robotContainer;
 
+	@Logged
 	private final PowerDistribution pdh = new PowerDistribution();
-	/**
-	 * Input voltage of the PDH.
-	 */
-	@Logged
-	private double voltage;
-
-	/**
-	 * Total current draw of the PDH.
-	 */
-	@Logged
-	private double totalCurrent;
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
@@ -73,8 +63,6 @@ public class Robot extends TimedRobot {
 		// and running subsystem periodic() methods. This must be called from the robot's periodic
 		// block in order for anything in the Command-based framework to work.
 		CommandScheduler.getInstance().run();
-		voltage = pdh.getVoltage();
-		totalCurrent = pdh.getTotalCurrent();
 	}
 
 	/** This function is called once each time the robot enters Disabled mode. */
