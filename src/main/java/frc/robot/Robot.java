3b88c4bf68c9044e0ec1fd2afc5b2dc802818aac
roboblazers7617 <roboblazers@universityhighschool.org>
2025-02-15 17:15:53 -0500

    Comment out the PDH logging
    Co-authored-by: Apollo Nargang <CoffeeCoder1@outlook.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,8 +31,8 @@ public class Robot extends TimedRobot {
 	 */
 	private final RobotContainer robotContainer;
 
-	@Logged
-	private final PowerDistribution pdh = new PowerDistribution();
+	// @Logged
+	// private final PowerDistribution pdh = new PowerDistribution();
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
