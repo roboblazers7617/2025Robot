4a0f48e4e6e7685c858b4a69c8cf51167bf06820
meepleBot <99357909+meepleBot@users.noreply.github.com>
2025-02-19 15:08:29 -0500

    Code review with TODOs and Issues entered in GitHub

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -36,6 +36,7 @@ public class Elevator extends SubsystemBase {
 	/**
 	 * The FeedForward used for the elevator.
 	 */
+	// TODO: This is using the wrong feedforward gains
 	private final ElevatorFeedforward elevatorFeedforward = new ElevatorFeedforward(ElevatorConstants.KS, ElevatorConstants.KV, ElevatorConstants.KA);
 	/**
 	 * The right elevator motor.
@@ -142,6 +143,7 @@ public class Elevator extends SubsystemBase {
 			safeElevatorTarget = ElevatorConstants.SAFE_MIN_POSITION;
 		}
 		double elevatorFeedForwardValue = elevatorFeedforward.calculate(elevatorEncoder.getVelocity()); // this is technically supposed to be the velocity setpoint
+		// TODO: #128 Pass in Units for Elevator FF
 		leaderElevatorMotor.getClosedLoopController().setReference(safeElevatorTarget, ControlType.kMAXMotionPositionControl, ClosedLoopSlot.kSlot0, elevatorFeedForwardValue);
 
 		double safeWristTarget = wristTarget;
@@ -150,7 +152,10 @@ public class Elevator extends SubsystemBase {
 		if (elevatorEncoder.getPosition() < ElevatorConstants.SAFE_MIN_POSITION && safeWristTarget < WristConstants.SAFE_MIN_POSITION) {
 			safeWristTarget = WristConstants.SAFE_MIN_POSITION;
 		}
+		// TODO: #130 Control upper bound of wrist
+
 		double wristFeedForwardValue = wristFeedforward.calculate(Math.toRadians(elevatorEncoder.getPosition()), Math.toRadians(elevatorEncoder.getVelocity()));
+		// TODO: #129 Pass in Units for Wrist FF
 		wristMotor.getClosedLoopController().setReference(safeWristTarget, ControlType.kMAXMotionPositionControl, ClosedLoopSlot.kSlot0, wristFeedForwardValue);
 	}
 
@@ -162,10 +167,12 @@ public class Elevator extends SubsystemBase {
 	 * @return
 	 *         {@link Command} to run.
 	 */
+	// TODO: #125 Change units of parameter
 	public Command setElevatorSpeedCommand(DoubleSupplier speed) {
 		Command command = new Command() {
 			@Override
 			public void execute() {
+				// TODO: #124 Check for Min_velocity as well (if going backward)
 				double targetSpeed = speed.getAsDouble();
 				if (targetSpeed > ElevatorConstants.MAX_VELOCITY) {
 					targetSpeed = ElevatorConstants.MAX_VELOCITY;
@@ -186,10 +193,12 @@ public class Elevator extends SubsystemBase {
 	 * @return
 	 *         {@link Command} to run.
 	 */
+	// TODO: #126 Change Units for Parameter for setWristSpeedCommand
 	public Command setWristSpeedCommand(DoubleSupplier speed) {
 		Command command = new Command() {
 			@Override
 			public void execute() {
+				// TODO: #127 Check for minimum value for wrist speed
 				double targetSpeed = speed.getAsDouble();
 				if (targetSpeed > WristConstants.MAX_VELOCITY) {
 					targetSpeed = WristConstants.MAX_VELOCITY;
