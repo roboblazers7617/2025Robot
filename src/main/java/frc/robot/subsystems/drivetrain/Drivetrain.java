70388385167f43ec5fe9deb11c7c0d36626b1629
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-13 22:40:15 -0400

    Import BooleanSupplier in Drivetrain oops

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -43,6 +43,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 import org.json.simple.parser.ParseException;
