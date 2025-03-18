b3b04e2fc1f485030f32a1489903b4cc3b0d4e11
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-18 00:04:55 -0400

    Add the mode LED to the ButtonBox firmware

--- ButtonBoxArduino/ButtonBoxArduino.ino
+++ ButtonBoxArduino/ButtonBoxArduino.ino
@@ -1,3 +1,4 @@
+#include <FastLED.h>
 #include <Control_Surface.h> // Include the Control Surface library
 #include "CCAbsoluteEncoderLooped.hpp"
 
@@ -8,6 +9,12 @@ const uint8_t JOYSTICK_SW = D22;
 const uint8_t ENCODER_CLK = D21;
 const uint8_t ENCODER_DT = D20;
 const uint8_t ENCODER_SW = D19;
+const uint8_t NEOPIXEL_PIN = D24;
+const uint8_t NEOPIXEL_COUNT = 1;
+
+// Define the array of leds.
+Array<CRGB, NEOPIXEL_COUNT> leds {};
+bool ledsDirty = false;
 
 // Instantiate a MIDI over USB interface.
 USBMIDI_Interface midi;
@@ -29,10 +36,31 @@ NoteButton joystickButton {
   0,
 };
 
+// Custom MIDI callback that prints incoming SysEx messages.
+struct MyMIDI_Callbacks : MIDI_Callbacks {
+  // This callback function is called when a SysEx message is received.
+  void onSysExMessage(MIDI_Interface &, SysExMessage sysex) override {
+    // Print the message
+    if (sysex.isCompleteMessage() && sysex.length == 13) {
+      leds[0] = CRGB(sysex.data[9], sysex.data[10], sysex.data[11]);
+    }
+    ledsDirty = true;
+  }
+} callback {};
+
 void setup() {
+  FastLED.addLeds<NEOPIXEL, NEOPIXEL_PIN>(leds.data, leds.length);
+  FastLED.setCorrection(TypicalPixelString);
+
   Control_Surface.begin(); // Initialize Control Surface
+  midi.setCallbacks(callback);
 }
 
 void loop() {
   Control_Surface.loop(); // Update the Control Surface
+
+  if (ledsDirty) { // If the colors changed
+    FastLED.show(); // Update the LEDs with the new colors
+    ledsDirty = false; // Clear the dirty flag
+  }
 }
