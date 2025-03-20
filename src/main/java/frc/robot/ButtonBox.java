4ed54e1975a4c4081649df063ed14eb3f4b280b1
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-19 20:33:48 -0400

    Color cycle the mode LEDs when disabled

--- src/main/java/frc/robot/ButtonBox.java
+++ src/main/java/frc/robot/ButtonBox.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.roboblazers7617.buttonbox.ButtonBoxServer;
@@ -14,6 +15,10 @@ public class ButtonBox extends SubsystemBase {
 	 * LED used to show coral/algae state.
 	 */
 	private final LEDMulticolor modeLED;
+	/**
+	 * The current hue of the {@link #modeLED}'s color cycling.
+	 */
+	private double hue = 0;
 
 	/**
 	 * RobotContainer used to poll subsystem states.
@@ -37,14 +42,22 @@ public class ButtonBox extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-		switch (robotContainer.getGamepieceMode()) {
-			case CORAL_MODE:
-				modeLED.setColor(Color.kWhite);
-				break;
+		if (DriverStation.isDisabled()) {
+			// Rainbow color cycle
+			modeLED.setColor(Color.fromHSV((int) hue, 255, 255));
+			hue += 0.25;
+		} else if (DriverStation.isAutonomousEnabled()) {
+			modeLED.setColor(Color.kRed);
+		} else if (DriverStation.isTeleopEnabled()) {
+			switch (robotContainer.getGamepieceMode()) {
+				case CORAL_MODE:
+					modeLED.setColor(Color.kWhite);
+					break;
 
-			case ALGAE_MODE:
-				modeLED.setColor(new Color(0, 1, 0.64));
-				break;
+				case ALGAE_MODE:
+					modeLED.setColor(new Color(0, 1, 0.64));
+					break;
+			}
 		}
 	}
 
